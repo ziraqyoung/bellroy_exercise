794d36339733b85e74243d338beca356a3dfd3c4
ziraq young <ziraqyoung@outlook.com>
2021-05-15 12:47:02 +0300

    Update text_parser spec to pass textfile filename to parse

--- spec/text_parser_spec.rb
+++ spec/text_parser_spec.rb
@@ -3,7 +3,7 @@ require 'text_parser'
 describe "BigFiveResults::TextParser" do
   describe "#read" do
     it 'returns formatted facets hash data' do
-      hash_of_models_with_facets = BigFiveResults::TextParser.new.read
+      hash_of_models_with_facets = BigFiveResults::TextParser.new(filename: "results.txt").read
 
       models = hash_of_models_with_facets.keys
       expect(models).to eq([:"EXTRAVERSION", :"AGREEABLENESS", :"CONSCIENTIOUSNESS", :"NEUROTICISM", :"OPENNESS TO EXPERIENCE"])
